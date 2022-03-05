e8acdec9d88c23f18a093522635cd71ca33afd57
Bruno Wolff <1767830+MrBrunoWolff@users.noreply.github.com>
2022-03-04 22:52:50 -0300

    initial(minimal-parcel): add files for Minimal Parcel structure

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import './style.css'
+
+const app = document.querySelector<HTMLDivElement>('#app')!
+
+app.innerHTML = `
+  <h1>Hello Parcel!</h1>
+  <a href="https://parceljs.org/docs/" target="_blank">Documentation</a>
+`
